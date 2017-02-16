35abd65ae2654c791beecfcd4838a292225ea424
KEN-LAPTOP\Ken <ken.itus92@gmail.com>
2017-02-16 19:53:32 +0700

    milestone 7: init view and event

--- app/src/main/java/apidez/com/lab1/activity/LoginActivity.java
+++ app/src/main/java/apidez/com/lab1/activity/LoginActivity.java
@@ -2,17 +2,37 @@ package apidez.com.lab1.activity;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import apidez.com.lab1.R;
 
 public class LoginActivity extends AppCompatActivity {
 
+    private TextView tvSignUp;
+    private TextView tvForgotPassword;
+    private EditText edtEmail, edtPassword;
+    private Button btnLogin;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        // TODO: Insert your code
+
+        tvSignUp = (TextView) findViewById(R.id.tvSignUp);
+        tvForgotPassword = (TextView) findViewById(R.id.tvForgotPassword);
+        edtEmail = (EditText) findViewById(R.id.edtEmail);
+        edtPassword = (EditText) findViewById(R.id.edtPassword);
+        btnLogin = (Button) findViewById(R.id.btnLogin);
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showToast(getString(R.string.message_of_our_team));
+            }
+        });
     }
 
     private void showToast(String message) {
