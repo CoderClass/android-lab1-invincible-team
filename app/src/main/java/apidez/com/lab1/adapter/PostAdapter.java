fd64d3d623dfbeddd4f087c815df38a7a98d91af
Phong Nguyen <phongnguyen180993@gmail.com>
2017-02-16 20:10:17 +0700

    Phong - load image into image view

--- app/src/main/java/apidez/com/lab1/adapter/PostAdapter.java
+++ app/src/main/java/apidez/com/lab1/adapter/PostAdapter.java
@@ -8,67 +8,64 @@ import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
-
-import java.util.List;
-
 import apidez.com.lab1.R;
 import apidez.com.lab1.model.Post;
+import com.squareup.picasso.Picasso;
+import java.util.List;
 
 /**
  * Created by nongdenchet on 10/9/16.
  */
 
 public class PostAdapter extends ArrayAdapter<Post> {
-    private List<Post> mPosts;
+  private List<Post> mPosts;
 
-    public PostAdapter(Context context, List<Post> posts) {
-        super(context, -1);
-        mPosts = posts;
+  public PostAdapter(Context context, List<Post> posts) {
+    super(context, -1);
+    mPosts = posts;
+  }
+
+  @Override public int getCount() {
+    return mPosts.size();
+  }
+
+  @NonNull @Override public View getView(int position, View convertView, ViewGroup parent) {
+    ViewHolder viewHolder = null;
+    if (convertView == null) {
+      viewHolder = new ViewHolder();
+      convertView = LayoutInflater.from(getContext()).inflate(R.layout.item_post, parent, false);
+      viewHolder.ivAvatar = (ImageView) convertView.findViewById(R.id.ivAvatar);
+      viewHolder.ivImage = (ImageView) convertView.findViewById(R.id.ivImage);
+      viewHolder.tvUsername = (TextView) convertView.findViewById(R.id.tvUsername);
+      viewHolder.tvDate = (TextView) convertView.findViewById(R.id.tvDate);
+      viewHolder.tvDescription = (TextView) convertView.findViewById(R.id.tvDescription);
+      convertView.setTag(viewHolder);
+    } else {
+      viewHolder = (ViewHolder) convertView.getTag();
     }
+    bindViewHolder(position, viewHolder);
 
-    @Override
-    public int getCount() {
-        return mPosts.size();
-    }
+    return convertView;
+  }
 
-    @NonNull
-    @Override
-    public View getView(int position, View convertView, ViewGroup parent) {
-        ViewHolder viewHolder = null;
-        if (convertView == null) {
-            viewHolder = new ViewHolder();
-            convertView = LayoutInflater.from(getContext()).inflate(R.layout.item_post, parent, false);
-            viewHolder.ivAvatar = (ImageView) convertView.findViewById(R.id.ivAvatar);
-            viewHolder.ivImage = (ImageView) convertView.findViewById(R.id.ivImage);
-            viewHolder.tvUsername = (TextView) convertView.findViewById(R.id.tvUsername);
-            viewHolder.tvDate = (TextView) convertView.findViewById(R.id.tvDate);
-            viewHolder.tvDescription = (TextView) convertView.findViewById(R.id.tvDescription);
-            convertView.setTag(viewHolder);
-        } else {
-            viewHolder = (ViewHolder) convertView.getTag();
-        }
-        bindViewHolder(position, viewHolder);
+  private void bindViewHolder(int position, ViewHolder viewHolder) {
+    Post post = mPosts.get(position);
+    viewHolder.tvUsername.setText(post.getUsername());
+    viewHolder.tvDate.setText(post.getDate());
+    viewHolder.tvDescription.setText(post.getDescription());
+    loadImage(viewHolder.ivAvatar, post.getAvatar());
+    loadImage(viewHolder.ivImage, post.getImage());
+  }
 
-        return convertView;
-    }
+  private void loadImage(ImageView imageView, String path) {
+    Picasso.with(getContext()).load(path).placeholder(R.drawable.placeholder).into(imageView);
+  }
 
-    private void bindViewHolder(int position, ViewHolder viewHolder) {
-        Post post = mPosts.get(position);
-        viewHolder.tvUsername.setText(post.getUsername());
-        viewHolder.tvDate.setText(post.getDate());
-        viewHolder.tvDescription.setText(post.getDescription());
-        loadImage(viewHolder.ivAvatar, post.getAvatar());
-        loadImage(viewHolder.ivImage, post.getImage());
-    }
-
-    private void loadImage(ImageView imageView, String path) {
-    }
-
-    public class ViewHolder {
-        public TextView tvUsername;
-        public TextView tvDescription;
-        public TextView tvDate;
-        public ImageView ivAvatar;
-        public ImageView ivImage;
-    }
+  public class ViewHolder {
+    public TextView tvUsername;
+    public TextView tvDescription;
+    public TextView tvDate;
+    public ImageView ivAvatar;
+    public ImageView ivImage;
+  }
 }
